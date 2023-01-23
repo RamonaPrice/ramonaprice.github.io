b8ae8c3166477752fd80aaee709aa74a141da705
RamonaPrice <media@nlcc.ca>
2023-01-23 14:48:30 -0800

    Removed border radius added all JS.

--- pcochef/events.js
+++ pcochef/events.js
@@ -1,3 +1,9 @@
 $("head").append(
     "<link rel='stylesheet' href='https://ramonaprice.github.io/pcochef/events.min.css' />"
 );
+$("head").append(
+    '<script type="text/javascript" src="https://pcochef-static.s3.amazonaws.com/plusapi/js/css-events.js"></script>'
+);
+$("head").append(
+    '<script src="https://pcochef-static.s3.amazonaws.com/plusapi/js/htmx.min.js" defer="defer"></script>'
+);
