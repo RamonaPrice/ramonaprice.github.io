c95b254fa3fa3927bccab5af83a5dae96b056f88
RamonaPrice <media@nlcc.ca>
2023-01-27 11:13:30 -0800

    Prevent css caching

--- pcochef/events.js
+++ pcochef/events.js
@@ -1,9 +1,12 @@
-$("head").append(
-    "<link rel='stylesheet' href='https://ramonaprice.github.io/pcochef/events.min.css' />"
-);
-$("head").append(
-    '<script type="text/javascript" src="https://pcochef-static.s3.amazonaws.com/plusapi/js/css-events.js"></script>'
-);
-$("head").append(
-    '<script src="https://pcochef-static.s3.amazonaws.com/plusapi/js/htmx.min.js" defer="defer"></script>'
-);
+(function($, window, document) {
+var v = Date.now();
+    $("head").append(
+        "<link rel='stylesheet' href='https://ramonaprice.github.io/pcochef/events.min.css?v="+v+"' />"
+    );
+    $("head").append(
+        '<script type="text/javascript" src="https://pcochef-static.s3.amazonaws.com/plusapi/js/css-events.js"></script>'
+    );
+    $("head").append(
+        '<script src="https://pcochef-static.s3.amazonaws.com/plusapi/js/htmx.min.js" defer="defer"></script>'
+    );
+})($, window, document);
