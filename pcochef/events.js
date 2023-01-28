026125cdca9082a6fbc0a27e244da06885b9d51b
RamonaPrice <media@nlcc.ca>
2023-01-27 16:46:50 -0800

    Add documentation

--- pcochef/events.js
+++ pcochef/events.js
@@ -1,9 +1,28 @@
 /*************************************
  * events.js
- * Author: Caleb
  * Organization: NLCC
- * Comments: Need to add a function to strip &tags=... within hx-ext.
+ * Notes: This addes the PCOChef CSS Event Embed (https://pcochef.com/plus/embeds/css-events/)
+ * Embed this script, and https://pcochef-static.s3.amazonaws.com/plusapi/js/htmx.min.js
+ * Add and addapt the following HTML:
+ * <div 
+ *    hx-get="https://pcochef.com/plusapi/o8lxG8Q/hxevents/?style=ts&amp;tags=[TAGS]&amp;filter=this_month" 
+ *    hx-trigger="load" 
+ *    hx-params="*" 
+ *    hx-swap="innerHTML" 
+ *    hx-ext="openurl" 
+ *    hx-openurl="/campus-events/yorkson-events" >
+ *    <img class="htmx-indicator" src="https://htmx.org/img/bars.svg" alt="Result loading..." width="150" />
+ * </div>
  * 
+ * Attributes:
+ *    hx-get: The PCO Chef API access point. Add the queries: tags=word+word, filter=this_month
+ *            Multiple tags can be combined with AND by using tags=tag1,tag2
+ *    hx-trigger, hx-params, hx-swap: Leave as is
+ *    hx-ext: Set this to "openurl" if you need to rewrite the base URL to go to the main events page
+ *    hx-openurl: Set this to the URL you want all the links to be re-written to. Also strips tags.
+ *                For example, if you set this to "/events" then this will occur:
+ *                "/lifetogether?event_id=32923985&tags=life+together" > "/events?event_id=32923985"
+ *    hx-keeptags: Add this if you don't want hx-openurl to strip the &tags=... from the URL
  */
 
 (function($, window, document) {
